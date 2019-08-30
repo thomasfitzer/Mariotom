4c809b77ae92227aa9ea0de53a5216062f88b330
hockey6409 <thomasfitzer89@gmail.com>
2019-08-30 15:39:18 -0500

    Reducers

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import ProjectSummary from './ProjectSummary'
 
-const ProjectList = () => {
+const ProjectList = ({projects}) => {
     return (
         <div className="project-list section">
-            <ProjectSummary />
-            <ProjectSummary />
-            <ProjectSummary />
-            <ProjectSummary />
+            { projects && projects.map(project => {
+                return (
+                    <ProjectSummary project={project} key={project.id} />
+                )
+            })}
         </div>
     )
 }
 
 export default ProjectList;
+
+// Line 7- This line is saying "if there are projects then do the map thing. The && is the key.
