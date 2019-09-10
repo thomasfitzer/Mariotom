11c4c75ca13fc826410c290694c6994bd9f3c9d7
hockey6409 <thomasfitzer89@gmail.com>
2019-09-10 10:07:09 -0500

    in and out links

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,22 +4,25 @@ import SignedInLinks from './SignedInLinks'
 import SignedOutLinks from './SignedOutLinks'
 import { connect } from 'react-redux'
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const { auth } = props;
+    // console.log(auth);
+    // helps us show either signed in or signed out links
+    const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />
     return (
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
                 <Link to='/' className="brand-logo">MarioPlan</Link>
-                <SignedInLinks />
-                <SignedOutLinks />
+                { links }
             </div>
         </nav>  
     )
 }
 
 const mapStatetoProps = (state) => {
-    console.log(state)
+    
     return {
-        
+       auth: state.firebase.auth 
     }
 }
 export default connect(mapStatetoProps)(Navbar)
