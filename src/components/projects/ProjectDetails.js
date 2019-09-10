8183375ebbf5c3398e55a7a316deaafc22f77bcb
hockey6409 <thomasfitzer89@gmail.com>
2019-09-10 10:59:22 -0500

    route guards

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -2,9 +2,15 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
+import { Redirect } from 'react-router-dom'
+
 
 const ProjectDetails = (props) => {
-    const { project } = props;
+    const { project, auth } = props;
+    
+    if (!auth.uid) return <Redirect to='/signin' />
+
+        // helps protect signed in links from people who are not signed in 
     if (project) {
         return (
         <div className="container section project-details">
@@ -36,7 +42,8 @@ const mapStateToProps = (state, ownProps) => {
     const projects = state.firestore.data.projects;
     const project = projects ? projects[id] : null
     return {
-        project: project
+        project: project,
+        auth: state.firebase.auth
     }
 }
 
