ba952f9f3b2ca30282a5edeba8541e6b95ecf1cb
hockey6409 <thomasfitzer89@gmail.com>
2019-09-10 11:50:40 -0500

    signup finalized

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { signUp } from '../../store/actions/authActions'
 
 
 class SignUp extends Component {
@@ -17,7 +18,8 @@ class SignUp extends Component {
     }
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state)
+        console.log(this.state);
+        this.props.signUp(this.state)
     }
     render() {
         const { authError, auth } = this.props;
@@ -35,15 +37,19 @@ class SignUp extends Component {
                         <input type="password" id="password" onChange={this.handleChange}/>
                     </div>
                     <div className="input-field">
-                        <label htmlFor="lastName">Last Name</label>
-                        <input type="text" id="lastName" onChange={this.handleChange}/>
-                    </div>
-                    <div className="input-field">
                         <label htmlFor="firstName">First Name</label>
                         <input type="text" id="firstName" onChange={this.handleChange}/>
                     </div>
                     <div className="input-field">
+                        <label htmlFor="lastName">Last Name</label>
+                        <input type="text" id="lastName" onChange={this.handleChange}/>
+                    </div>
+                    <div className="input-field">
                         <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
+                        <div className="red-text center">
+                            { authError ? <p>{ authError }</p> : null}
+                            {/* if signup has an error, this will show a message */}
+                        </div>
                     </div>
                 </form>
             </div>
@@ -53,8 +59,15 @@ class SignUp extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        authError: state.auth.authError
     }
 }
 
-export default connect(mapStateToProps)(SignUp)
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signUp: (newUser) => dispatch(signUp(newUser))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp)
